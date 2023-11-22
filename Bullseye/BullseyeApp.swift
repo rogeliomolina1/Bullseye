b7f22b6c9384c3e093d8b7a21d9237a301f67689
Rogelio <rogelio.molina1@gmail.com>
2023-11-21 17:55:33 -0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Rogelio Molina on 11/21/23.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
