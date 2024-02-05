9eecc30192dacd8f7da29e846d5b01988ead9d37
Rogelio <rogelio.molina1@gmail.com>
2024-02-05 15:14:33 -0800

    Starter code

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -9,13 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+      VStack {
             Text("Hello, world!")
         }
-        .padding()
     }
 }
 
